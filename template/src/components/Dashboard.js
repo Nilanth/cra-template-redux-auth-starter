13bf87005f062043be526fb1e86dee4d7bdb82cf
Nilanth <nilanth@mallow-tech.com>
2021-01-26 16:24:02 +0530

    Update Dashboard.js

--- template/src/components/Dashboard.js
+++ template/src/components/Dashboard.js
@@ -6,7 +6,7 @@ function Dashboard() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    CRA Template PreConfigured with
+                    Create React App Template Preconfigured with
                 </p>
                 <ol style={{ textAlign: 'left' }}>
                     <li>
@@ -25,7 +25,7 @@ function Dashboard() {
                         Redux Saga
                     </li>
                     <li>
-                        Basic Authentication
+                        Basic Authentication / Routes
                     </li>
                     <li>
                         SASS
@@ -33,11 +33,11 @@ function Dashboard() {
                 </ol>
                 <a
                     className="App-link"
-                    href="https://reactjs.org"
+                    href="https://github.com/Nilanth/cra-template-redux-auth-starter"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Learn More
                 </a>
             </header>
         </div>
